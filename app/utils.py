e30ebe6b0b60343c1dfb2acd9e02818fb7a61f30
Jonnattan Griffiths <jonnattan@gmail.com>
2024-12-18 22:16:35 +0000

    Se corrige implementación de cifrado aes

--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,9 @@ try:
     import json
     import pymysql.cursors
     from datetime import datetime
-    from jose import jwe # https://pypi.org/project/python-jose/
+    from Crypto.Cipher import AES
+    import base64 #https://www.pycryptodome.org/src/examples#encrypt-data-with-aes
+
 except ImportError:
     logging.error(ImportError)
     print((os.linesep * 2).join(['[Deposits] Error al buscar los modulos:', str(sys.exc_info()[1]), 'Debes Instalarlos para continuar', 'Deteniendo...']))
@@ -116,52 +118,68 @@ class Bank() :
         self.id = -1
 
 class Cipher() :
-    aes_key = ''
-    algorithm = ''
-
-    def __init__(self, algorithm='A256GCM') :
-        self.id = id
-        self.aes_key = 'AAAAASSSSSDDDDDDFFFFFGGGGGHHHHJJJJJHHHGGGFFDDSDSSSSSDDDFFFGGGHHHGGFDD' # 256 bit
-        self.algorithm = algorithm
+    cipher = None
+    aes_key = None
+    iv = b'1234567890123456'
+    def __init__(self, ) :
+        key = os.environ.get('AES_KEY','None')
+        self.aes_key = key.encode('utf-8')[:32]
 
     def __del__(self):
-        self.aes_key = ''
-        self.algorithm = ''
+        self.aes_key = None
+        
+    def complete( self, data_str : str ) :
+        response : str = data_str
+        if data_str != None :
+            length = len(data_str)
+            resto = 16 - (length % 16)
+            i = 0
+            while i < resto :
+                response += " "
+                i += 1
+        return response.encode()
 
-    def aes_encrypt(self, payload ) :
-        data_cipher = None
+    def aes_encrypt(self, payload : str ) :  
+        data_cipher_str = None
         try :
-            key = self.aes_key.encode('utf-8')[:32]
-            plaintext = str(payload).encode('utf-8')
-            logging.info("Cifro " + str(self.algorithm))
-            # data_cipher = jwe.encrypt(plaintext=plaintext, key=key, algorithm='A256KW', encryption='A256CBC-HS512')
+            data_clear = self.complete(payload) # se lleva a bytes el texto
+            cipher = AES.new(self.aes_key, AES.MODE_CBC, self.iv)
+            data_cipher = cipher.encrypt(data_clear) # se encriptan los bytes
+            if data_cipher != None :
+                b64 = base64.b64encode(data_cipher) # se convierten en base64
+                data_cipher_str = b64.decode() # pasan a string la cadena de bytes
         except Exception as e:
             print("ERROR Cipher:", e)
-            data_cipher = None
-        return data_cipher
+            data_cipher_str = None
+        return data_cipher_str
 
-    def aes_decrypt(self, data ) :
-        data_clear = None
+    def aes_decrypt(self, data_cipher_str: str ) :        
+        data_clear_str = None
         try :
-            logging.info("Decifro " + str(self.algorithm))
-            data_clear = jwt.decrypt(data, self.aes_key, algorithm=[self.algorithm] )
+            b64 = data_cipher_str.encode() # string se pasan a bytes
+            data_cipher = base64.b64decode(b64) # bytes en base64 se pasan a los bytes para decifrar
+            cipher = AES.new(self.aes_key, AES.MODE_CBC, self.iv)
+            data_clear = cipher.decrypt(data_cipher) # se desencriptan los bytes
+            if data_clear != None :
+                data_clear_str = data_clear.decode() # se llega la cadeba de bytes a texto
         except Exception as e:
             print("ERROR Decipher:", e)
-            data_clear = None
-        return data_clear
+            data_clear_str = None
+        return data_clear_str
     
     def test( self, request ) : 
         response_data = {"message":"NOk", "data": None }
         http_code = 400
-        logging.info("Reciv Header : " + str(request.headers) )
-        logging.info("Reciv " + str(request.method) )
+        # logging.info("Reciv Header : " + str(request.headers) )
+        # logging.info("Reciv " + str(request.method) )
         logging.info("Reciv Data: " + str(request.data) )
         request_data = request.get_json()
         user = request_data['user']
         passwd = request_data['password']
-        clean_text = {"data": str(user) + "|||" + str(passwd)}
+        clean_text = str(user) + "|||" + str(passwd)
         aes_encrypt = self.aes_encrypt( clean_text )
+        logging.info("Decifrada : " + str(self.aes_decrypt( str(aes_encrypt) )) )
         if aes_encrypt != None :
-            response_data = {"message":"Ok", "data": aes_encrypt }
+            response_data = {"message":"Ok", "data": str(aes_encrypt) }
             http_code = 200
         return response_data, http_code
